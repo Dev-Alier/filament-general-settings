4c5ab23934f43ecc037486d1c79024d73931ed1c
ALi ER <111887741+Dev-Alier@users.noreply.github.com>
2025-10-27 17:55:32 +0300

    Update filament-general-settings.php

--- config/filament-general-settings.php
+++ config/filament-general-settings.php
@@ -6,6 +6,7 @@ return [
     'show_analytics_tab' => true,
     'show_seo_tab' => true,
     'show_email_tab' => true,
+    'show_sms_tab' => true,
     'show_social_networks_tab' => true,
     'expiration_cache_config_time' => 60,
 ];
