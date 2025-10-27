32bdb7562ca6721d8b3b4305e23b83d078151d6f
ALi ER <111887741+Dev-Alier@users.noreply.github.com>
2025-10-27 16:48:29 +0300

    Add SmsDataHelper class for SMS configuration

---
+++ src/Helpers/SmsDataHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Joaopaulolndev\FilamentGeneralSettings\Helpers;
+
+class SmsDataHelper
+{
+    public static function getSmsConfigFromDatabase(array $data): array
+    {
+        $newValues = $data;
+
+        $newValues['netgsm_username'] = $data['sms_settings']['netgsm_username'] ?? null;
+        $newValues['netgsm_password'] = $data['sms_settings']['netgsm_password'] ?? null;
+        $newValues['netgsm_header'] = $data['sms_settings']['netgsm_header'] ?? null;
+
+        return $newValues;
+    }
+
+    public static function setSmsConfigToDatabase($data): mixed
+    {
+        $data['email_settings'] = [
+            'netgsm_username' => $data['netgsm_username'] ?? null,
+            'netgsm_password' => $data['netgsm_password'] ?? null,
+            'netgsm_header' => $data['netgsm_header'] ?? null,
+        ];
+
+        return $data;
+    }
+}
