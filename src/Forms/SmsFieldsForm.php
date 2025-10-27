3cef12bab46a97ad7f9b6c836ca3605419f4fccb
ALi ER <111887741+Dev-Alier@users.noreply.github.com>
2025-10-27 16:59:10 +0300

    Create SmsFieldsForm class for SMS settings

---
+++ src/Forms/SmsFieldsForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Joaopaulolndev\FilamentGeneralSettings\Forms;
+
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+
+class SmsFieldsForm
+{
+    public static function get(): array
+    {
+        return [
+            TextInput::make('netgsm_username')
+                ->label(__('filament-general-settings::default.netgsm_username')),
+            TextInput::make('netgsm_password')
+                ->label(__('filament-general-settings::default.netgsm_password')),
+            TextInput::make('netgsm_header')
+                ->label(__('filament-general-settings::default.google_header')),
+        ];
+    }
+}
